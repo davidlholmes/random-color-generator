c0f8743deda895f9fe1b05297707054635733c9b
David <dholme25@gmail.com>
2022-02-14 20:11:22 -0500

    Generate multiple random numbers in js file.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const button = document.getElementsByTagName("button");
-
-button.addEventListener("click", chooseRandomColor());
+const button = document.getElementById("button");
 
 function chooseRandomColor() {
-	
+	let random1 = Math.round(Math.random() * 255);
+	let random2 = Math.round(Math.random() * 255);
+	let random3 = Math.round(Math.random() * 255);
+	console.log(random1);
+	console.log(random2);
+	console.log(random3);
 }
+
+button.addEventListener("click", chooseRandomColor());
