2da830ee21161d83062095cb5787619e76b60fec
David <dholme25@gmail.com>
2022-02-19 01:21:46 -0500

    Got the console to stop complaining but the random color is still not applying.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const button = document.getElementById("button");
+let button = document.getElementById("button");
 
-function chooseRandomColor() {
+button.addEventListener('click', () => {
 	let random1 = Math.round(Math.random() * 255);
 	let random2 = Math.round(Math.random() * 255);
 	let random3 = Math.round(Math.random() * 255);
 	console.log(random1);
 	console.log(random2);
 	console.log(random3);
-}
-
-button.addEventListener("click", chooseRandomColor());
+	let bg = document.querySelector('main');
+	bg.style.color = "rgb(random1, random2, random3)";
+})
