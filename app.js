9e3b4d45fa09d26bfbe08a0c433ad797c1bf9b0e
David Holmes <davidholmes@Davids-MBP.lan>
2022-03-21 08:28:41 -0400

    Fumbling about trying to make it work.

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 let chooseRandomButton = document.getElementById("chooseRandomButton");
-
 let bg = document.getElementsByClassName('color');
-
+let paragraph = "";
 let randomArray = [];
+let hexDigits = [];
+let redInput = document.querySelector("#redInput");
+let blueInput = document.querySelector("#blueInput");
+let greenInput = document.querySelector("#greenInput");
+let redDisplay = document.querySelector("#redDisplay");
+let blueDisplay = document.querySelector("#blueDisplay");
+let greenDisplay = document.querySelector("#greenDisplay");
+let submit = document.querySelector("#submitSliders");
+
+function decToHex(decNum) {
+	i = 0;
+	while (decNum > 0) {
+		hexDigits[i] = decNum % 16;
+		if (hexDigits[i] = 10) {
+			hexDigits[i] = 'A';
+		} else if (hexDigits[i] = 11) {
+			hexDigits[i] = 'B';
+		} else if (hexDigits[i] = 12) {
+			hexDigits[i] = 'C';
+		} else if (hexDigits[i] = 13) {
+			hexDigits[i] = 'D';
+		} else if (hexDigits[i] = 14) {
+			hexDigits[i] = 'E';
+		} else if (hexDigits[i] = 15) {
+			hexDigits[i] = 'F';
+		}
+		decNum = Math.floor(decNum / 16);
+		i++;
+	}
+	return hexDigits.reverse().concat();
+}
+
 
 chooseRandomButton.addEventListener('click', () => {
 	for (i = 0; i < 3; i++) {
@@ -15,24 +46,20 @@ chooseRandomButton.addEventListener('click', () => {
 
 	if (!!document.getElementById("paragraph")) {
 		document.querySelector("#hiddenPG").removeChild(paragraph);
-	  let paragraph = document.createElement("p");
-  	paragraph.setAttribute("id", "paragraph");
-	  paragraph.innerText = `The selected color value is rgb(${randomArray[0]}, ${randomArray[1]}, ${randomArray[2]}).`;
-	} else {
-		let paragraph = document.createElement("p");
-  	paragraph.setAttribute("id", "paragraph");
-	  paragraph.innerText = `The selected color value is rgb(${randomArray[0]}, ${randomArray[1]}, ${randomArray[2]}).`;
-		document.querySelector("#hiddenPG").appendChild(paragraph);
 	}
-})
 
-let redInput = document.querySelector("#redInput");
-let blueInput = document.querySelector("#blueInput");
-let greenInput = document.querySelector("#greenInput");
-let redDisplay = document.querySelector("#redDisplay");
-let blueDisplay = document.querySelector("#blueDisplay");
-let greenDisplay = document.querySelector("#greenDisplay");
-let submit = document.querySelector("#submitSliders");
+	let paragraph = document.createElement("p");
+	paragraph.setAttribute("id", "paragraph");
+	function pgText(a, b, c) {
+		paragraph.innerText = `The selected color value is rgb(${a}, ${b}, ${c}). In hexadecimal, this would be`;
+	}
+	pgText(randomArray[0], randomArray[1], randomArray[2]);
+	document.querySelector("#hiddenPG").appendChild(paragraph);
+}
+)
+
+
+
 
 redInput.oninput = () => {
 	redDisplay.style.backgroundColor = `rgb(${redInput.value}, 0, 0)`;
