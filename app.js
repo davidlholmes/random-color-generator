9a035e2085c648b52f2a461374f3c8d40e8ad2e7
David Holmes <davidholmes@Davids-MBP.lan>
2022-03-28 19:27:28 -0400

    I can't keep up with everything I've tried.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let chooseRandomButton = document.getElementById("chooseRandomButton");
+let submit = document.querySelector("#submitSliders");
 let bg = document.getElementsByClassName('color');
 let paragraph = "";
 let randomArray = [];
@@ -9,31 +10,40 @@ let greenInput = document.querySelector("#greenInput");
 let redDisplay = document.querySelector("#redDisplay");
 let blueDisplay = document.querySelector("#blueDisplay");
 let greenDisplay = document.querySelector("#greenDisplay");
-let submit = document.querySelector("#submitSliders");
+
 
 function decToHex(decNum) {
 	i = 0;
 	while (decNum > 0) {
 		hexDigits[i] = decNum % 16;
-		if (hexDigits[i] = 10) {
-			hexDigits[i] = 'A';
-		} else if (hexDigits[i] = 11) {
-			hexDigits[i] = 'B';
-		} else if (hexDigits[i] = 12) {
-			hexDigits[i] = 'C';
-		} else if (hexDigits[i] = 13) {
-			hexDigits[i] = 'D';
-		} else if (hexDigits[i] = 14) {
-			hexDigits[i] = 'E';
-		} else if (hexDigits[i] = 15) {
-			hexDigits[i] = 'F';
+		if (hexDigits[i] === 10) {
+			hexDigits[i] = "A";
+		} else if (hexDigits[i] === 11) {
+			hexDigits[i] = "B";
+		} else if (hexDigits[i] === 12) {
+			hexDigits[i] = "C";
+		} else if (hexDigits[i] === 13) {
+			hexDigits[i] = "D";
+		} else if (hexDigits[i] === 14) {
+			hexDigits[i] = "E";
+		} else if (hexDigits[i] === 15) {
+			hexDigits[i] = "F";
 		}
 		decNum = Math.floor(decNum / 16);
 		i++;
 	}
-	return hexDigits.reverse().concat();
+	return hexDigits.reverse().join('');
 }
 
+function pgText(a, b, c, d) {
+	let paragraph = document.createElement("p");
+	paragraph.setAttribute("id", "paragraph");
+	paragraph.innerText = `${d} rgb(${a}, ${b}, ${c}). In hexadecimal, this would be #${decToHex(a)}${decToHex(b)}${decToHex(c)}.`;
+	document.querySelector("#hiddenPG").appendChild(paragraph);
+	if (document.querySelector("#hiddenPG").childNodes.length > 1) {
+		document.querySelector("#hiddenPG").firstChild.remove();
+	}
+}
 
 chooseRandomButton.addEventListener('click', () => {
 	for (i = 0; i < 3; i++) {
@@ -43,18 +53,8 @@ chooseRandomButton.addEventListener('click', () => {
 	for (i = 0; i < bg.length; i++) {
 		bg[i].style.backgroundColor = `rgb(${randomArray[0]}, ${randomArray[1]}, ${randomArray[2]})`;
 	}
-
-	if (!!document.getElementById("paragraph")) {
-		document.querySelector("#hiddenPG").removeChild(paragraph);
-	}
-
-	let paragraph = document.createElement("p");
-	paragraph.setAttribute("id", "paragraph");
-	function pgText(a, b, c) {
-		paragraph.innerText = `The selected color value is rgb(${a}, ${b}, ${c}). In hexadecimal, this would be`;
-	}
-	pgText(randomArray[0], randomArray[1], randomArray[2]);
-	document.querySelector("#hiddenPG").appendChild(paragraph);
+	
+	pgText(randomArray[0], randomArray[1], randomArray[2], "The random color value is");
 }
 )
 
@@ -62,20 +62,52 @@ chooseRandomButton.addEventListener('click', () => {
 
 
 redInput.oninput = () => {
-	redDisplay.style.backgroundColor = `rgb(${redInput.value}, 0, 0)`;
+	
+	let paragraph = document.createElement("p");
+	paragraph.innerText = `${redInput.value}`;
+	document.querySelector("#redDisplay").appendChild(paragraph);
+	if (document.querySelector("#redDisplay").childNodes.length > 1) {
+		document.querySelector("#redDisplay").firstChild.remove();
+	}
+
+
+	//redDisplay.style.backgroundColor = `rgb(${redInput.value}, 0, 0)`;
 }
 
 greenInput.oninput = () => {
-	greenDisplay.style.backgroundColor = `rgb(0, ${greenInput.value}, 0)`;
+	let paragraph = document.createElement("p");
+	paragraph.innerText = `${greenInput.value}`;
+	document.querySelector("#greenDisplay").appendChild(paragraph);
+	if (document.querySelector("#greenDisplay").childNodes.length > 1) {
+		document.querySelector("#greenDisplay").firstChild.remove();
+	}
 }
 
 blueInput.oninput = () => {
-	blueDisplay.style.backgroundColor = `rgb(0, 0, ${blueInput.value})`;
+	let paragraph = document.createElement("p");
+	paragraph.innerText = `${blueInput.value}`;
+	document.querySelector("#blueDisplay").appendChild(paragraph);
+	if (document.querySelector("#blueDisplay").childNodes.length > 1) {
+		document.querySelector("#blueDisplay").firstChild.remove();
+	}
+	
+	//blueDisplay.style.backgroundColor = `rgb(0, 0, ${blueInput.value})`;
 }
 
 submit.addEventListener('click', () => {
-	let bg = document.getElementsByClassName('color');
 	for (i = 0; i < bg.length; i++) {
 		bg[i].style.backgroundColor = `rgb(${redInput.value}, ${greenInput.value}, ${blueInput.value})`;
+		
+	pgText(redInput.value, greenInput.value, blueInput.value, "You chose the color value");
 	}
 })
+/*
+chooseRandomButton.addEventListener('click', () => {
+	for (i = 0; i < bg.length; i++) {
+		bg[i].style.backgroundColor = `rgb(${randomArray[0]}, ${randomArray[1]}, ${randomArray[2]})`;
+	}
+	
+	pgText(randomArray[0], randomArray[1], randomArray[2], "The selected color value is");
+}
+)
+*/
